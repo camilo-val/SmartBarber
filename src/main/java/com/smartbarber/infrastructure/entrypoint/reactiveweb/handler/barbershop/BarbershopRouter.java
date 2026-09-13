5895dc81e5822e4b757b049ce73e6691c7398bc2
Gabriela Cardona <camilo.val1999@gmail.com>
2026-09-12 23:45:09 -0500

    Se corrigen rutas de barbershop router

--- src/main/java/com/smartbarber/infrastructure/entrypoint/reactiveweb/handler/barbershop/BarbershopRouter.java
+++ src/main/java/com/smartbarber/infrastructure/entrypoint/reactiveweb/handler/barbershop/BarbershopRouter.java
@@ -17,7 +17,7 @@ public class BarbershopRouter {
     public RouterFunction<ServerResponse> routerBarbershop(){
         return RouterFunctions.route(RequestPredicates.POST(BARBER_SERVICE + "/create-barber"), barbershopHandler::createBarbershop)
                 .andRoute(RequestPredicates.GET(BARBER_SERVICE + "/name/{name}"), barbershopHandler::findBarbershopByNae)
-                .andRoute(RequestPredicates.GET(BARBER_SERVICE + "/companyName/{companyName}"), barbershopHandler::findBarbershopByCompanyName)
+                .andRoute(RequestPredicates.GET(BARBER_SERVICE + "/company-name/{companyName}"), barbershopHandler::findBarbershopByCompanyName)
                 .andRoute(RequestPredicates.GET(BARBER_SERVICE + "/document/{document}"), barbershopHandler::findByBarbershopByDocument)
                 .andRoute(RequestPredicates.GET(BARBER_SERVICE + "/id/{id}"), barbershopHandler::findBarbershopById)
                 .andRoute(RequestPredicates.PUT(BARBER_SERVICE + "/update-barber/{id}"), barbershopHandler::updateBarbershop);
